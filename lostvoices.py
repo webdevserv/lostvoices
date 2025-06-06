7422ad49fdaad51ce5628824b87dafd5d1a542b7
webdevserv <60757009+webdevserv@users.noreply.github.com>
2025-06-06 23:37:16 +0200

    Update lostvoices.py

--- lostvoices.py
+++ lostvoices.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 meta_tags = """
 <head>
     <meta name="description" content="Streamlit app Dark Waters: A tribute to silenced voices, reflecting on loss, erasure, and justice.">
-    <meta name="keywords" content="Nataly Wood, Richard Wagner, Women violence, injustice, conceptual art">
+    <meta name="keywords" content="Nataly Wood, Richard Wagner homicide, Women violence, injustice, conceptual art, violence">
     <meta name="author" content="Your Name">
 </head>
 """
@@ -54,8 +54,8 @@ selected_portrait = random.choice(portraits)
 portrait = cv2.imread(selected_portrait)
 
 if portrait is not None:
-    portrait_resized = resize_image(portrait, 800, 800)
-    water_resized = resize_image(water_texture, 800, 800)
+    portrait_resized = resize_image(portrait, 400, 400)
+    water_resized = resize_image(water_texture, 400, 400)
     image_placeholder.image(cv2.cvtColor(portrait_resized, cv2.COLOR_BGR2RGB), use_container_width=True)
     time.sleep(3)
 
@@ -65,8 +65,8 @@ while True:
     portrait = cv2.imread(selected_portrait)
 
     if portrait is not None:
-        portrait_resized = resize_image(portrait, 800, 800)
-        water_resized = resize_image(water_texture, 800, 800)
+        portrait_resized = resize_image(portrait, 400, 400)
+        water_resized = resize_image(water_texture, 400, 400)
 
         for alpha in np.linspace(0, 1, num=20):
             blended = cv2.addWeighted(portrait_resized, 1 - alpha, water_resized, alpha, 0)
